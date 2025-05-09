6158eb82a521b33cdc06e6b3d8894aa3c5a4faee
Clintonawa10 <umaclintonawa@gmail.com>
2025-05-09 19:49:28 +0100

    MultiplicationTable.java

--- multiplicationTable.java
+++ multiplicationTable.java
@@ -4,7 +4,7 @@ public class multiplicationTable {
         Scanner input = new Scanner(System.in); //Creating an object of the Scanner class
         System.out.print("Enter a number: ");
         int num = input.nextInt();  
-        for (int i=1; i<=10; i++) {  
+        for (int i=1; i<=12; i++) {  
             System.out.println(num + " x " + i + " = " + (num*i));  
         }  
     }  
