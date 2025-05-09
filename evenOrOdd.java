5e43a63ad48fae7368220245653d68bbe3c1703f
Clintonawa10 <umaclintonawa@gmail.com>
2025-05-09 19:45:43 +0100

    EvenOrOdd.java

--- evenOrOdd.java
+++ evenOrOdd.java
@@ -1,9 +1,10 @@
 import java.util.Scanner; //importing the Scanner class
 public class evenOrOdd {
     public static void main(String args[]) {
-        Scanner input = new Scanner(System.in); //Creating an object of the Scanner class
+        Scanner input = new Scanner(System.in); //Creates the Scanner class input
         System.out.print("Enter a number: ");
-        int num = input.nextInt();  //Enters number
-        System.out.println(num % 2 == 0 ? "The number is Even" : "The number is Odd");  
+        int num = input.nextInt();  //to enter a number
+        System.out.println(num % 2 == 0 ? "The number is Even" : "The number is Odd");
+        //to show if the number is even or odd
     }
 }
